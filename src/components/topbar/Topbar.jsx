143236ac609291364486e8a7eea6d8a98629b684
devinvogt2 <vogtdevin@ymail.com>
2022-04-14 02:51:17 -0500

    add: added icons for email and phone number

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./topbar.scss"
+import { Person, Mail } from "@material-ui/icons"
 
 function Topbar() {
     return (
@@ -7,7 +8,14 @@ function Topbar() {
             <div className='wrapper'>
                 <div className='left'>
                     <a href='#intro' className='logo'>Mr. Vogt</a>
-                    <h1>Hello</h1>
+                    <div className='iconContainer'>
+                        <Person className='icon' />
+                        <span>405-822-2110</span>
+                    </div>
+                    <div className='iconContainer'>
+                        <Mail className='icon' />
+                        <span>vogtdevinbusiness@gmail.com</span>
+                    </div>
                 </div>
                 <div className='right'>
                     this is right
