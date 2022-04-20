2168135d8c2db49d3f71a934835786ad4b3adfc1
devinvogt2 <vogtdevin@ymail.com>
2022-04-19 22:15:52 -0500

    add: ityped useEffect/useRef needs a couple bug fixes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
 import "./intro.scss"
+import { init } from "ityped"
+import { useEffect, useRef } from 'react'
 import { ArrowDownward } from "@material-ui/icons";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: false,
+            strings: ["Developer", "Designer", "Content Creator"],
+        });
+    }, []);
+
     return (
         <div className='intro' id='intro'>
             <div className='left'>
@@ -14,7 +25,9 @@ export default function Intro() {
                 <div className='wrapper'>
                     <h2>Hi There, I'm</h2>
                     <h1>Devin Vogt</h1>
-                    <h3>Web<span>Developer</span></h3>
+                    <h3>
+                        Web <span ref={textRef}></span>
+                    </h3>
                 </div>
                 <a href='#portfolio'>
                     <ArrowDownward className='icon' />
