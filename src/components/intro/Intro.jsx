8ed7202ec09992a4c3f68e7c836443b60fbacfd9
devinvogt2 <vogtdevin@ymail.com>
2022-04-19 21:21:19 -0500

    add: img of self to intro page with animations

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./intro.scss"
+import { ArrowDownward } from "@material-ui/icons";
 
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
-
+            <div className='left'>
+                <div className='imgContainer'>
+                    <img src="assets/devin-trans-background.png" alt='' />
+                </div>
+            </div>
+            <div className='right'>
+                <div className='wrapper'>
+                    <h2>Hi There, I'm</h2>
+                    <h1>Devin Vogt</h1>
+                    <h3>Web<span>Developer</span></h3>
+                </div>
+                <a href='#portfolio'>
+                    <ArrowDownward className='icon' />
+                </a>
+            </div>
         </div>
     )
 }
